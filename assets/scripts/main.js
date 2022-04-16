c25466d43cc8d88af86129751afdcf7fa7771dbb
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-04-16 10:50:31 +0530

    Updated: navbar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,5 +3,5 @@ window.addEventListener("scroll", styleNav);
 function styleNav()
 {
     // Adding styles to navbar while scroll
-    document.querySelector("header").classList.toggle("nav-scroll", window.scrollY > 0);
+    document.querySelector(".coloured-section").classList.toggle("nav-scroll", window.scrollY > 0);
 }
